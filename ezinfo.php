29043de5ef438b320c72b775f90b3e1496d7dda0
Peter Keung <peter@mugo.ca>
2013-02-21 10:18:04 -0800

    Update copyright info

---
+++ ezinfo.php
@@ -0,0 +1,21 @@
+<?php
+
+class mugorssInfo
+{
+    /**
+     * info
+     *
+     * @access public
+     * @return void
+     */
+    static function info()
+    {
+        return array( 'Name'      => "<a href='http://www.mugo.ca'>Mugo RSS</a>",
+                      'Version'   => '1.0',
+                      'Copyright' => "Copyright (C) <a href='http://www.mugo.ca'>Mugo Web</a>",
+                      'License'   => 'GNU General Public License v2.0',
+                    );
+    }
+}
+
+?>
